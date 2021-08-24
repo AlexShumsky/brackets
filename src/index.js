e62b0f44bb9a2e16c1f4676d2867cea0b6681b70
AlexShumsky <sanya.shumskiy.95@mail.ru>
2021-08-24 07:45:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config = bracketsConfig.map(val => val.join(''));
+  let result;
+
+  function getResult(str) {
+    let newStr = str;
+    config.forEach(brackets => {
+      str = str.replace(brackets, '');
+    });
+    if (newStr.length === str.length) {
+      result = str;
+    } else getResult(str);
+  }
+  getResult(str);
+  return !result;
 }
